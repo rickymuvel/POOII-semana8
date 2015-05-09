a19cf37c762de84952f611a3daf79d96cc38a445
yo <yo@localhost>
2015-05-08 22:04:01 -0500

    Envio cambios

---
+++ Semana 8/Viernes/APP_DESCONECTADO/APP_DESCONECTADO/ClienteDA.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration.ConfigurationManager
+
+Public Class ClienteDA
+    Dim cn As New SqlConnection(ConnectionStrings("cno").ConnectionString)
+    Dim da As New SqlDataAdapter
+    Dim ds As New DataSet
+
+    Public Function ClienteListar() As DataSet
+        da = New SqlDataAdapter("USP_CLIENTELISTAR_DS", cn)
+        da.SelectCommand.CommandType = CommandType.StoredProcedure
+        ds = New DataSet
+        da.Fill(ds, "Clientes")
+        ds.Tables("Clientes").PrimaryKey =
+            New DataColumn() {ds.Tables("Clientes").Columns(0)}
+        Return ds
+    End Function
+
+
+End Class
---
+++ Semana 8/Viernes/APP_DESCONECTADO/APP_DESCONECTADO/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("APP_DESCONECTADO")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("APP_DESCONECTADO")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("dd0d304e-582c-4ea2-af1e-5a002472ca78")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
